af20f7a98eda7af14c767ec2b03bfe54bc347984
Muthu kumaran M <kumaran2852@gmail.com>
2026-06-06 14:23:14 +0530

    port hard

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ app.get("/api/sample-movies", (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 8020, () => {
+    console.log(`Server running on port ${process.env.PORT || 8020}`);
 });
